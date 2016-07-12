e424c4c4a6c370fff62ddf4b2e34ed2b38d5e7d3
lq <xiaohutuxian5212@163.com>
2016-07-12 15:39:24 +0800

    1

---
+++ vdq/src/main/java/com/tianyi/yw/dao/TaskTimeMapper.java
@@ -0,0 +1,26 @@
+package com.tianyi.yw.dao;
+
+import java.util.List;
+
+import com.tianyi.yw.model.TaskTime;
+
+@MyBatisRepository
+public interface TaskTimeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TaskTime record);
+
+    int insertSelective(TaskTime record);
+
+    TaskTime selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(TaskTime record);
+
+    int updateByPrimaryKey(TaskTime record);
+
+	List<TaskTime> getTaskTimeList(TaskTime taskTime);
+	
+	int getTaskTimeCount(TaskTime record);
+
+	List<TaskTime> getAllTaskTimeList(Integer taskId);
+}
